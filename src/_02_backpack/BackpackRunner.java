7dab14767435678ffefdae1c103a1901cfa6615a
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2020-08-19 11:27:56 -0700

    werw

--- src/_02_backpack/BackpackRunner.java
+++ src/_02_backpack/BackpackRunner.java
@@ -2,6 +2,17 @@ package _02_backpack;
 
 public class BackpackRunner {
 	public static void main(String[] args) {
-		new Backpack().packAndCheck();
+		
+		
+		Backpack pack = new Backpack();
+		Pencil pencil = new Pencil();
+		Ruler ruler = new Ruler();
+		Textbook textbook = new Textbook();
+		
+	pack.putInBackpack(pencil);
+	pack.putInBackpack(ruler);
+	pack.putInBackpack(textbook);
+	pack.packAndCheck();
+		
 	}
 }
